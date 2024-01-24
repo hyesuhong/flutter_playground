90a84c8fdd122390f02b85125461a0a4fbb470c9
Hyesu <honghs95@gmail.com>
2024-01-24 10:39:10 +0900

    feat: money_app - add card area

--- money_app/lib/main.dart
+++ money_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:money_app/widgets/button.dart';
+import 'package:money_app/widgets/button.dart' show Button;
 
 void main() {
   runApp(const MyApp());
@@ -40,6 +40,7 @@ class MyApp extends StatelessWidget {
                       Text(
                         'Welcome back',
                         style: TextStyle(
+                          fontSize: 12,
                           color: Colors.white.withOpacity(0.7),
                         ),
                       ),
@@ -79,9 +80,106 @@ class MyApp extends StatelessWidget {
                     label: 'Request',
                     backgroundColor: Color(0xff1f2123),
                     textColor: Colors.white,
-                  )
+                  ),
                 ],
               ),
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  const Text(
+                    'Wallets',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    'View All',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.7),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  color: Color(0xff1f2123),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                    topRight: Radius.elliptical(60, 40),
+                    bottomLeft: Radius.elliptical(60, 40),
+                  ),
+                ),
+                padding: const EdgeInsets.only(
+                  top: 24,
+                  bottom: 32,
+                  left: 16,
+                  right: 16,
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Euro',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          children: [
+                            const Text(
+                              '6 428',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              'EUR',
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.7),
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Transform(
+                      transform: Matrix4.identity()
+                        ..translate(-5.0, 30.0)
+                        ..scale(2.5, 2.5, 2.5),
+                      alignment: Alignment.center,
+                      child: const Icon(
+                        Icons.euro,
+                        color: Colors.white,
+                        size: 80,
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
