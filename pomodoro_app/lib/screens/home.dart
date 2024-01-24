cc8e48f781e6205761df8ece8ca7ce988ab0b9c0
Hyesu <honghs95@gmail.com>
2024-01-24 15:29:14 +0900

    feat: pomodoro_app - add timer

--- pomodoro_app/lib/screens/home.dart
+++ pomodoro_app/lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,6 +10,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int totalSeconds = 1500;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds--;
+    });
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(
+        seconds: 1,
+      ),
+      onTick,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +38,7 @@ class _HomeState extends State<Home> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                '25:00',
+                totalSeconds.toString(),
                 style: TextStyle(
                     color: Theme.of(context).cardColor,
                     fontSize: 88,
@@ -33,7 +53,7 @@ class _HomeState extends State<Home> {
                 iconSize: 112,
                 color: Theme.of(context).cardColor,
                 icon: const Icon(Icons.play_circle_outline),
-                onPressed: () {},
+                onPressed: () => onStartPressed(),
               ),
             ),
           ),
@@ -44,6 +64,9 @@ class _HomeState extends State<Home> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Theme.of(context).cardColor,
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(50),
+                      ),
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
