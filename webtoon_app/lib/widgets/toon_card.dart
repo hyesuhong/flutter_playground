58ea32aee36273514ea3a0f151519368c40d29e5
Hyesu <honghs95@gmail.com>
2024-01-25 17:08:46 +0900

    feat: webtoon_app - divide thumbnail class

--- webtoon_app/lib/widgets/toon_card.dart
+++ webtoon_app/lib/widgets/toon_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:webtoon_app/models/toon_model.dart';
 import 'package:webtoon_app/screens/detail.dart';
 import 'package:webtoon_app/styles/font.dart';
+import 'package:webtoon_app/widgets/thumbnail.dart';
 
 class ToonCard extends StatelessWidget {
   final ToonModel toon;
@@ -19,6 +20,8 @@ class ToonCard extends StatelessWidget {
           MaterialPageRoute(
             builder: (context) => DetailScreen(
               id: toon.id,
+              title: toon.title,
+              thumb: toon.thumb,
             ),
           ),
         );
@@ -26,27 +29,7 @@ class ToonCard extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 10,
-                  offset: const Offset(0, 8),
-                  color: Colors.black.withOpacity(0.3),
-                )
-              ],
-            ),
-            child: Image.network(
-              toon.thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
-            ),
-          ),
+          Thumbnail(thumbUrl: toon.thumb),
           const SizedBox(
             height: 16,
           ),
