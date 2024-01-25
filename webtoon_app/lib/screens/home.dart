0083858ec0fac369f04c54a3d8cd30aa1d9dd708
Hyesu <honghs95@gmail.com>
2024-01-25 16:12:56 +0900

    feat: webtoon_app - render webtoon card

--- webtoon_app/lib/screens/home.dart
+++ webtoon_app/lib/screens/home.dart
@@ -30,14 +30,13 @@ class Home extends StatelessWidget {
         future: toons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              separatorBuilder: (context, index) => const SizedBox(width: 20),
-              itemBuilder: (context, index) {
-                var toon = snapshot.data![index];
-                return Text(toon.title);
-              },
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 24,
+                ),
+                Expanded(child: makeToonList(snapshot)),
+              ],
             );
           } else if (snapshot.hasError) {
             return const Text('Error');
@@ -47,6 +46,51 @@ class Home extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  ListView makeToonList(AsyncSnapshot<List<ToonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+      itemCount: snapshot.data!.length,
+      separatorBuilder: (context, index) => const SizedBox(width: 40),
+      itemBuilder: (context, index) {
+        var toon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    offset: const Offset(0, 8),
+                    color: Colors.black.withOpacity(0.3),
+                  )
+                ],
+              ),
+              child: Image.network(
+                toon.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              toon.title,
+              style: const TextStyle(
+                  fontSize: 16, fontVariations: [NotoSansKRWeight.w400]),
+            )
+          ],
+        );
+      },
     );
   }
 }
