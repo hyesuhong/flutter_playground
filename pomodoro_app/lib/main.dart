808023b4e4a26eb7ea2132712d2c5a4e9ff159a0
Hyesu <honghs95@gmail.com>
2024-02-14 17:25:29 +0900

    fix: pomodoro_app - match app class name with test file

--- pomodoro_app/lib/main.dart
+++ pomodoro_app/lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:pomodoro_app/screens/home.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const MyApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
